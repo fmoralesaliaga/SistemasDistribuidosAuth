cfc1ce763da05767959b8105f216c12013188cdf
fmoralesaliaga <fmoralesaliaga@gmail.com>
2023-12-04 07:55:39 -0300

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,70 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
+import React from 'react';
 
 export default function Login() {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  function saveToken(db: any, userToken: any) {
+    // create a new transaction
+    const txn = db.transaction('userToken', 'readwrite');
+
+    // get the Contacts object store
+    const store = txn.objectStore('userToken');
+    //
+    let query = store.put(userToken);
+
+    // handle success case
+    query.onsuccess = function (event: any) {
+      console.log(event);
+    };
+
+    // handle the error case
+    query.onerror = function (event: any) {
+      console.log(event.target.errorCode);
+    };
+
+    // close the database once the
+    // transaction completes
+    txn.oncomplete = function () {
+      db.close();
+    };
+  }
+
+  function Login() {
+    const request = indexedDB.open('Sesion', 6);
+    request.onupgradeneeded = () => {
+      let db = request.result;
+      if (!db.objectStoreNames.contains('token')) {
+        db.createObjectStore('token', { keyPath: 'key' });
+      }
+    };
+
+    var myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+
+    var raw = JSON.stringify({
+      username: username,
+      password: password,
+    });
+
+    fetch('http://localhost:3001/api/login', {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+    })
+      .then((response) => response.text())
+      .then((result) => {
+        const objectStore = db.transaction.objectStore('toDoList');
+
+        console.log(result);
+      })
+      .catch((error) => console.log('error', error));
+  }
+
   return (
     <>
       <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
@@ -10,18 +73,21 @@ export default function Login() {
         </div>
 
         <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
-          <form className='space-y-6' action='#' method='POST'>
+          <div className='space-y-6'>
             <div>
-              <label htmlFor='email' className='block text-sm font-medium leading-6 text-gray-900'>
-                Correo Electrónico
+              <label htmlFor='username' className='block text-sm font-medium leading-6 text-gray-900'>
+                Nombre de Usuario
               </label>
               <div className='mt-2'>
                 <input
-                  id='email'
-                  name='email'
-                  type='email'
-                  autoComplete='email'
+                  id='username'
+                  name='username'
+                  type='username'
+                  autoComplete='username'
                   required
+                  onChange={(e) => {
+                    setUsername(e.target.value);
+                  }}
                   className=' pl-2  block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                 />
               </div>
@@ -44,6 +110,9 @@ export default function Login() {
                   name='password'
                   type='password'
                   autoComplete='current-password'
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
                   required
                   className='pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                 />
@@ -53,12 +122,15 @@ export default function Login() {
             <div>
               <button
                 type='submit'
+                onClick={() => {
+                  Login();
+                }}
                 className='flex w-full justify-center rounded-md bg-black px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
               >
                 Identificate
               </button>
             </div>
-          </form>
+          </div>
 
           <p className='mt-10 text-center text-sm text-gray-500'>
             ¿Aun no eres miembro?{' '}
